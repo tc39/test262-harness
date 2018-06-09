0d5240d334bc7762390c92776975e4f3a87217e8
Mike Pennisi <mike@mikepennisi.com>
2018-06-09 15:41:25 -0400

    Complete implementation of `--acceptVersion`

--- test/test.js
+++ test/test.js
@@ -111,3 +111,39 @@ function validateResultRecords(records, options = { prelude: false }) {
     });
   });
 }
+
+tap.test('unsupported version without `acceptVersion`', (test) => {
+  run(['test/collateral-unsupported-version/test/**/*.js'])
+    .then(() => {
+      test.fail('Expected command to fail, but it succeeded.');
+    }, () => {})
+    .then(test.done);
+});
+
+tap.test('supported version with matching `acceptVersion`', (test) => {
+  run(['test/collateral-supported-version/test/**/*.js',  '--acceptVersion', '3.0.0'])
+    .catch(test.fail)
+    .then(test.done);
+});
+
+tap.test('supported version with non-matching `acceptVersion`', (test) => {
+  run(['test/collateral-supported-version/test/**/*.js',  '--acceptVersion', '99.0.0'])
+    .then(() => {
+      test.fail('Expected command to fail, but it succeeded.');
+    }, () => {})
+    .then(test.done);
+});
+
+tap.test('unsupported version with matching `acceptVersion`', (test) => {
+  run(['test/collateral-unsupported-version/test/**/*.js',  '--acceptVersion', '99.0.0'])
+    .catch(test.fail)
+    .then(test.done);
+});
+
+tap.test('unsupported version with non-matching `acceptVersion`', (test) => {
+  run(['test/collateral-unsupported-version/test/**/*.js',  '--acceptVersion', '98.0.0'])
+    .then(() => {
+      test.fail('Expected command to fail, but it succeeded.');
+    }, () => {})
+    .then(test.done);
+});
