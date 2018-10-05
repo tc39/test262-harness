e10abaec733a2e31d0a987a9ab1d9a752de7cbf4
Rick Waldron <waldron.rick@gmail.com>
2018-10-05 17:33:55 -0400

    Fix typo

--- test/collateral-with-harness/loose-tests/my-test.js
+++ test/collateral-with-harness/loose-tests/my-test.js
@@ -5,7 +5,7 @@ info: >
   projects may include Test262 tests prior to their introduction to the Test262
   repository.
 
-  Like the xample in the `test262` directory, this test signals correct
+  Like the example in the `test262` directory, this test signals correct
   behavior by throwing an EvalError to avoid false positives in cases where the
   source text is not evaluated.
 flags: [onlyStrict]
