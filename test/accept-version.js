3d22ae9de8765331df2c5cc44d1ae5ae9a6da9ed
Rick Waldron <waldron.rick@gmail.com>
2019-04-29 11:41:50 -0400

    Defer version enforcement checks to test262-stream
    Removes legacy version validation code. This is now handled by test262-stream and should not be double handled by test262-harness

--- test/accept-version.js
+++ test/accept-version.js
@@ -6,9 +6,7 @@ const reporter = 'json';
 
 tap.test('unsupported version without `acceptVersion`', assert => {
   run(['test/collateral-unsupported-version/test/**/*.js'], {reporter})
-    .then(() => {
-      assert.fail('Expected command to fail, but it succeeded.');
-    }, () => {})
+    .catch(assert.fail)
     .then(assert.done);
 });
 
@@ -20,9 +18,7 @@ tap.test('supported version with matching `acceptVersion`', assert => {
 
 tap.test('supported version with non-matching `acceptVersion`', assert => {
   run(['test/collateral-supported-version/test/**/*.js',  '--acceptVersion', '99.0.0'], {reporter})
-    .then(() => {
-      assert.fail('Expected command to fail, but it succeeded.');
-    }, () => {})
+    .catch(assert.fail)
     .then(assert.done);
 });
 
